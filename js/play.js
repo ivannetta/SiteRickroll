93919335a08d8dc5ddc45dda7996bfa54420c9e9
usrDottik <dotcomnose@gmail.com>
2022-05-11 16:14:19 -0300

    Fix some stuff... I think

--- js/play.js
+++ js/play.js
@@ -1,6 +1,6 @@
 function play() {
     let div = document.getElementById("tmpInteract");
-    let video = document.getElementById("importantvideo");
+    let video = document.getElementById("video");
     div.remove();
     video.removeAttribute("hidden")
     video.play();
